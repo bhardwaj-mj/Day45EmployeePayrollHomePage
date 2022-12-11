13de4d4a6b368903b8f026262eccb659f02a5f4b
bhardwaj-mj <bhardwajmanoj555@gmail.com>
2022-12-11 13:32:09 +0530

    Added: display employee details from json object

--- new_home.js
+++ new_home.js
@@ -3,24 +3,53 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 const createInnerHtml = () => {
-    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" +
-        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" + 
+                        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+    let empPayrollData = createEmployeePayrollJSON()[0];                    
     const innerHtml = `${headerHtml}
      <tr>
       <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -2.png"></td>
-      <td>Manoj Kumar</td>
-      <td>Male</td>
+      <td>${empPayrollData._name}</td>
+      <td>${empPayrollData._gender}</td>
+      <td><div class='dept=label'>${empPayrollData._department[0]}</div><div class='dept-label'>${empPayrollData._department[1]}</div></td>
+      <td>${empPayrollData._salary}</td>
+      <td>${empPayrollData._gender}</td>
       <td>
-      <div class='dept=label'>HR</div>
-      <div class='dept-label'>Finance</div>
-      </td>
-      <td>3000000</td>
-      <td>1 Nov 2020</td>
-      <td>
-         <img id="1" onclick="remove(this)" alt="delete"src="../assets/icons/delete-black-18dp.svg">
-         <img id="1" alt="edit" onclick="update(this)"src="../assets/icons/create-black-18dp.svg">
+         <img name="${empPayrollData._id}" onclick="remove(this)" alt="delete"src="../assets/icons/delete-black-18dp.svg">
+         <img name="${empPayrollData._id}" alt="edit" onclick="update(this)"src="../assets/icons/create-black-18dp.svg">
       </td>    
 </tr>
     `;
     document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal =[
+        {
+            _name: 'Narayan Mahadevan',
+            _gender: 'male',
+            _department: [
+                'Engineering',
+                'Finance'
+            ],
+            _salary:'500000',
+            _startDate: '29 Oct 2019',
+            _note:'',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/profile-images/Ellipse -2.png'
+        },
+        {
+            _name: 'Rashmika Mandanna',
+            _gender: 'Female',
+            _department: [
+                'Finance'
+            ],
+            _salary:'5000000',
+            _startDate: '29 Sept 2020',
+            _note:'',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/profile-images/Ellipse -1.png'
+        }
+    ];
+    return empPayrollListLocal;
 }
